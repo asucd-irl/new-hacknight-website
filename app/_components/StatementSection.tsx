0c4579932c458d73f735f5be7765f12ffc42eb05
Daniel Kim <hyunkim03@gmail.com>
2025-05-27 16:32:08 -0700

    Statement section

--- app/_components/StatementSection.tsx
+++ app/_components/StatementSection.tsx
@@ -1,12 +1,15 @@
 import Section from './Section';
+import { TextReveal } from "@/components/magicui/text-reveal";
+
 
 export default function StatementSection() {
   return (
-    <Section id="statement" bg="bg-stone-500">
-      <p className="max-w-3xl text-center text-3xl font-black">
-        hack nights brings creative minds together to build and experiment with
-        new ideas.
-      </p>
+    <Section id="statement" bg="bg-[#7A40FF]">
+      <TextReveal>
+          hack nights brings creative minds together to build and experiment with
+          new ideas.
+        
+      </TextReveal>
     </Section>
   );
 }
